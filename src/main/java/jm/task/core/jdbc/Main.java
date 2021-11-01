6d3d7c560a81bc7cae02d0ac88a6d5a721e12d5b
Alexander Tyurin <alexturin@gmail.com>
2021-11-01 16:22:31 +0700

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+
+        userDao.saveUser("Василий", "Пупкин", (byte)50);
+        userDao.saveUser("Иван", "Иванов", (byte)23);
+        userDao.saveUser("Сергей", "Петров", (byte)35);
+        userDao.saveUser("Семен" , "Семенов", (byte)30);
+
+        List<User> users = userDao.getAllUsers();
+        for (User user : users){
+            System.out.println(user.getId() + " " + user.toString());
+        }
+
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,96 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
+public class UserDaoJDBCImpl implements UserDao{
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        try (Connection connection = Util.getMySQLConnection()) {
+            String sql = "CREATE TABLE USERS (id BIGINT not null AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(255) not null, " +
+                    "lastName VARCHAR(255) not null, " +
+                    "age TINYINT not null)";
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLSyntaxErrorException e){
+            return;
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try(Connection connection = Util.getMySQLConnection()) {
+            String sql = "DROP TABLE USERS";
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        }catch (SQLSyntaxErrorException e) {
+            return;
+        } catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+    public void saveUser(String name, String lastName, byte age){
+        try (Connection connection = Util.getMySQLConnection()){
+            Statement statement = connection.createStatement();
+            String sql = "INSERT INTO USERS( name, lastName, age) VALUES('"
+                    + name +"', '"
+                    +  lastName + "', "
+                    + age+ ")";
+            statement.executeUpdate(sql);
+            System.out.printf("User с именем %s - добавлен в базу \n", name);
+        } catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try(Connection connection = Util.getMySQLConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "DELETE FROM USERS WHERE id = " + id;
+            statement.executeUpdate(sql);
+        } catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> list= new ArrayList<>();
+        try(Connection connection = Util.getMySQLConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "SELECT id, name, lastName, age FROM USERS";
+            ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()){
+                User user = new User();
+                user.setId(rs.getLong("id"));
+                user.setName(rs.getString("name"));
+                user.setLastName(rs.getString("lastName"));
+                user.setAge(rs.getByte("age"));
+                list.add(user);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     public void cleanUsersTable() {
-
+        try(Connection connection = Util.getMySQLConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "TRUNCATE USERS";
+            statement.executeUpdate(sql);
+        } catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
